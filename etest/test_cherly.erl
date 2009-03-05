8b1fef2b944018b0b47d17a801935c669a402ef8
Cliff Moon <cliff@moonpolysoft.com>
2009-03-04 20:20:49 -0800

    test the lru from erlang

--- etest/test_cherly.erl
+++ etest/test_cherly.erl
@@ -6,33 +6,32 @@ simple_test() ->
   cherly:put(C, "key", Value),
   ?assertEqual(Value, cherly:get(C, "key")),
   ?assertEqual(16, cherly:size(C)),
-  ?debugHere,
   cherly:stop(C).
   
 put_get_and_remove_test() ->
-  ?debugHere,
   {ok, C} = cherly:start(120),
   Value = <<"value">>,
-  ?debugHere,
-  timer:sleep(10),
   ?assertEqual(not_found, cherly:get(C, "key")),
   cherly:put(C, "key", Value),
   ?assertEqual(Value, cherly:get(C, "key")),
   cherly:remove(C, "key"),
   ?assertEqual(not_found, cherly:get(C, "key")),
-  ?assertEqual(0, cherly:size(0)),
+  ?assertEqual(0, cherly:size(C)),
   cherly:stop(C).
   
+put_with_lru_eject_test() ->
+  {ok, C} = cherly:start(120),
+  Value = <<"value">>,
+  lists:foldl(fun(_, Str) ->
+      Mod = succ(Str),
+      cherly:put(C, Mod, Value),
+      Mod
+    end, "aaa", lists:seq(1, 10)),
+  ?assertEqual(112, cherly:size(C)),
+  ?assertEqual(7, cherly:items(C)),
+  cherly:stop(C).
   
-  
-  
-  
-  % lists:foldl(fun(_, Str) ->
-  %     
-  %   end, "aaa", lists:seq())
-  % cherly:put(C, "key", Value).
-  
-  
+
   
   
   
